cf5496f35758e4445f3b047f4d46008055f07686
David Smith <dnsmith124@gmail.com>
2022-06-24 11:27:57 -0500

    removing deprecated ReactDOM.render call and replaced with createRoot

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -17,7 +17,9 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store} from './Redux/store';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
@@ -98,8 +100,6 @@ ReactDOM.render(
       </Routes>
     </BrowserRouter>
   </Provider>
-  ,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
