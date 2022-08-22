7cd4c2f5b5e8ad0162c9bd6bf2aed0033da1b3e4
David Smith <dnsmith124@gmail.com>
2022-08-22 11:43:21 -0500

    removing obsolete drag and drop package

--- src/Components/QueryComponents/QueryBar.js
+++ src/Components/QueryComponents/QueryBar.js
@@ -1,6 +1,5 @@
 
 import React from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Button from "../FormFields/Button";
 import QueryItem from "../QueryComponents/QueryItem";
 
@@ -9,40 +8,6 @@ const QueryBar = ({handleSubmission, handleOnDragEnd, handleQueryItemChange,
 
   return (
     <form onSubmit={handleSubmission}>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="query-item" direction="horizontal">
-          {(provided) => (
-            <ul className="query-box" {...provided.droppableProps} ref={provided.innerRef}>
-              {storedQuery.map((item, index) => {
-                let itemIsNode = (item.type === "node") ? true : false;
-                return (
-                  <Draggable key={index} draggableId={index.toString()} index={index} >
-                    {(provided) => (
-                      <li 
-                        ref={provided.innerRef} 
-                        {...provided.draggableProps} 
-                        {...provided.dragHandleProps} 
-                        className="query-list-item">
-                        <QueryItem 
-                          item={item} 
-                          handleClose={()=>handleRemoveQueryItem(index)} 
-                          handleChange={handleQueryItemChange}
-                          hasInput={itemIsNode}
-                          name={item.name}
-                          inputKey={index}
-                          isSelected={item.selected}
-                          inputActive>
-                        </QueryItem>
-                      </li>
-                    )}
-                  </Draggable>
-                )
-              })}
-              {provided.placeholder}
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
       <Button type="submit" size="s" disabled={isLoading}>
         <span>Submit Query</span>
       </Button>
