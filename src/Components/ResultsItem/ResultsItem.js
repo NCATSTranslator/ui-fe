f8ff2d57b07c8d17521fb091e8b1e97163763c73
David Smith <dnsmith124@gmail.com>
2022-08-29 11:28:39 -0500

    small results changes

--- src/Components/ResultsItem/ResultsItem.js
+++ src/Components/ResultsItem/ResultsItem.js
@@ -64,7 +64,7 @@ const ResultsItem = ({key, item, allSelected, handleSelected, activateEvidence, 
       </button>
       <AnimateHeight 
         className={`${styles.accordionPanel} ${isExpanded ? styles.open : styles.closed } ${item.description ? styles.hasDescription : styles.noDescription }`}
-        duration={250}
+        duration={500}
         height={height}
         > 
         <div className={styles.container}>
