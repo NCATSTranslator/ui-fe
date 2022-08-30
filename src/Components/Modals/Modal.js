6f13838b263952b3296c657f23bafbe63c68b9ea
David Smith <dnsmith124@gmail.com>
2022-08-30 09:31:01 -0500

    increased size of modal close icon and functionality to close on esc keypress

--- src/Components/Modals/Modal.js
+++ src/Components/Modals/Modal.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from './ModalGlobals.module.scss';
 import {ReactComponent as Close} from '../../Icons/Buttons/Close.svg';
 
 const Modal = ({children, isOpen, onClose, className, containerClass, hideCloseButton}) => {
 
   const startOpen = (isOpen === undefined) ? false : isOpen;
-  var modalIsOpen = (startOpen) ? styles.true : styles.false ;
+  var modalIsOpen = (startOpen) ? styles.true : styles.false;
+
+  const handleKeypress = (e) => {
+    if(e.key === 'Escape') {
+      onClose();
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeypress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeypress);
+    };
+  }, []);
 
   return (
     <div className={`${styles.modalWindow} ${modalIsOpen} ${className}`}>
