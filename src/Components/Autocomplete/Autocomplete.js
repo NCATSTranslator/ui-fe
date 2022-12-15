b71cfe75de8705916216c1af9d8bc07fb322da2a
David Smith <dnsmith124@gmail.com>
2022-12-15 15:22:14 -0600

    added additional functionality to autocomplete bar, fixed loading anim

--- src/Components/Autocomplete/Autocomplete.js
+++ src/Components/Autocomplete/Autocomplete.js
@@ -1,7 +1,34 @@
 import styles from './Autocomplete.module.scss';
 import loadingIcon from '../../Assets/Images/Loading/loading-purple.png';
+import { useCallback, useState, useEffect } from 'react';
 
 const Autocomplete = ({isLoading, items, handleItemClick}) => {
+
+  const [numberVisibleItems, setNumberVisibleItems] = useState(0);
+  const pageLength = 10;
+
+  const showMoreItems = useCallback((e) => {
+    e.stopPropagation();
+    if(numberVisibleItems + pageLength >= items.length)
+      setNumberVisibleItems(items.length);
+    else
+      setNumberVisibleItems((prev) => prev + pageLength);
+
+  }, [numberVisibleItems]);
+
+  const showFewerItems = useCallback((e) => {
+    e.stopPropagation();
+    if(numberVisibleItems - pageLength <= pageLength)
+      setNumberVisibleItems(pageLength);
+    else
+      setNumberVisibleItems((prevNumVisItems) => prevNumVisItems - pageLength);
+
+  }, [numberVisibleItems]);
+
+  useEffect(() => {
+    let initNumberVisibleItems = (items.length > pageLength) ? pageLength : items.length;
+    setNumberVisibleItems(initNumberVisibleItems);
+  }, [items]);
 
   return (
     <div 
@@ -10,7 +37,7 @@ const Autocomplete = ({isLoading, items, handleItemClick}) => {
       >
       {
         items && !isLoading &&
-        items.map((item, i) => {
+        items.slice(0, numberVisibleItems).map((item, i) => {
           return <p key={i} className={styles.item} onClick={()=>handleItemClick(item)}>{item.label}</p>
         })
       }
@@ -18,7 +45,32 @@ const Autocomplete = ({isLoading, items, handleItemClick}) => {
         isLoading &&
         <img src={loadingIcon} className={styles.loadingIcon} alt="loading icon" />
       }
-    <div className={styles.sep}></div>
+    {
+      !isLoading &&
+      <div>
+        <div className={styles.sep}></div>
+        <div className={styles.buttonsContainer}>
+          {
+            items.length > pageLength && !isLoading &&
+            <button 
+              onClick={(e)=>showMoreItems(e)} 
+              className={`${styles.button} ${(numberVisibleItems < items.length) ? styles.active : styles.inactive}`} 
+              >
+              Show More
+            </button>
+          }
+          {
+            items.length > pageLength && !isLoading &&
+            <button 
+              onClick={(e)=>showFewerItems(e)} 
+              className={`${styles.button} ${(numberVisibleItems > pageLength) ? styles.active : styles.inactive}`}
+              >
+              Show Less
+            </button>
+          }
+        </div>
+      </div>
+    }
   </div>
   );
 }
