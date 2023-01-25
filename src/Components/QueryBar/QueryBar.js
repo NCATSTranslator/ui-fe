686c3026638cf23e00512f7933c70d726eefd083
David Smith <dnsmith124@gmail.com>
2023-01-25 12:23:50 -0600

    removing unnecessary console log

--- src/Components/QueryBar/QueryBar.js
+++ src/Components/QueryBar/QueryBar.js
@@ -31,7 +31,6 @@ const QueryBar = ({handleSubmission, handleChange, handleQueryTypeChange, isDisa
   }
 
   useEffect(() => {
-    console.log(presetTypeID)
     if(presetTypeID === null || presetTypeID === undefined) 
       return;
 
