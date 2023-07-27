6864f457a70686f7b2fa7dce42355412ff596b9d
David Smith <dnsmith124@gmail.com>
2023-07-27 15:34:49 -0500

    set up paths for new routing, configure share url generation to include pathname

--- src/Utilities/resultsInteractionFunctions.js
+++ src/Utilities/resultsInteractionFunctions.js
@@ -70,5 +70,5 @@ export const handleClearAllFilters = (asFilters, rResults, oResults, setActiveFi
 }
 
 export const getResultsShareURLPath = (label, nodeID, typeID, pk) => {
-  return `/results?l=${label}&i=${nodeID}&t=${typeID}&q=${pk}`
+  return `${window.location.pathname}/results?l=${label}&i=${nodeID}&t=${typeID}&q=${pk}`
 }
--- src/index.js
+++ src/index.js
@@ -31,88 +31,102 @@ import {store} from './Redux/store';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const routes = [
+  // {
+  //   path: "/",
+  //   element: <Page title="Home"><Home /></Page>
+  // },
+  {
+    path: "design",
+    element: <Page title="Design System"><DesignSystem /></Page>
+  },
+  {
+    path: "terms-of-use",
+    element: <Page title="Terms of Use"><Terms /></Page>
+  },
+  {
+    path: "help",
+    element: <FAQPage title="Frequently Asked Questions"><Help /></FAQPage>
+  },
+  {
+    path: "what-is-translational-science",
+    element: <FAQPage title="What is Translational Science"><WhatIs /></FAQPage>
+  },
+  {
+    path: "funding-information",
+    element: <FAQPage title="Funding Information"><Affiliates /></FAQPage>
+  },
+  {
+    path: "how-it-works",
+    element:<FAQPage title="How It Works"><HowItWorks /></FAQPage>
+  },
+  {
+    path: "knowledge-providers",
+    element: <FAQPage title="Knowledge Providers"><Kps /></FAQPage>
+  },
+  {
+    path: "autonomous-relay-agents",
+    element: <FAQPage title="Autonomous Relay Agents"><Aras /></FAQPage>
+  },
+  {
+    path: "autonomous-relay-system",
+    element: <FAQPage title="Autonomous Relay System"><Ars /></FAQPage>
+  },
+  {
+    path: "knowledge-graphs",
+    element: <FAQPage title="Knowledge Graphs"><Kgs /></FAQPage>
+  },
+  {
+    path: "smartapi",
+    element: <FAQPage title="SmartAPI"><SmartAPI /></FAQPage>
+  },
+  {
+    path: "forming-a-question",
+    element: <FAQPage title="Forming a Question"><Question /></FAQPage>
+  },
+  {
+    path: "article-results",
+    element: <FAQPage title="Results"><ResultsArticle /></FAQPage>
+  },
+  {
+    path: "evidence",
+    element: <FAQPage title="Evidence"><Evidence /></FAQPage>
+  },
+  {
+    path: "search-history",
+    element: <FAQPage title="Search History"><SearchHistoryArticle /></FAQPage>
+  },
+  {
+    path: "send-feedback",
+    element: <FAQPage title="Send Feedback"><SendFeedbackArticle /></FAQPage>
+  },
+  {
+    path: "results",
+    element: <Page title="Results"><Results /></Page>
+  },
+  {
+    path: "history",
+    element: <Page title="History"><History /></Page>
+  }
+]
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
-    children: [
+    children: [ 
       {
-        path: "/",
-        element: <Page title="Home"><Home /></Page>
+        path: "demo",
+        element: <Page title="Home"><Home /></Page>,
+        children: routes
       },
       {
-        path: "design",
-        element: <Page title="Design System"><DesignSystem /></Page>
-      },
-      {
-        path: "terms-of-use",
-        element: <Page title="Terms of Use"><Terms /></Page>
-      },
-      {
-        path: "help",
-        element: <FAQPage title="Frequently Asked Questions"><Help /></FAQPage>
-      },
-      {
-        path: "what-is-translational-science",
-        element: <FAQPage title="What is Translational Science"><WhatIs /></FAQPage>
-      },
-      {
-        path: "funding-information",
-        element: <FAQPage title="Funding Information"><Affiliates /></FAQPage>
-      },
-      {
-        path: "how-it-works",
-        element:<FAQPage title="How It Works"><HowItWorks /></FAQPage>
-      },
-      {
-        path: "knowledge-providers",
-        element: <FAQPage title="Knowledge Providers"><Kps /></FAQPage>
-      },
-      {
-        path: "autonomous-relay-agents",
-        element: <FAQPage title="Autonomous Relay Agents"><Aras /></FAQPage>
-      },
-      {
-        path: "autonomous-relay-system",
-        element: <FAQPage title="Autonomous Relay System"><Ars /></FAQPage>
-      },
-      {
-        path: "knowledge-graphs",
-        element: <FAQPage title="Knowledge Graphs"><Kgs /></FAQPage>
-      },
-      {
-        path: "smartapi",
-        element: <FAQPage title="SmartAPI"><SmartAPI /></FAQPage>
-      },
-      {
-        path: "forming-a-question",
-        element: <FAQPage title="Forming a Question"><Question /></FAQPage>
-      },
-      {
-        path: "article-results",
-        element: <FAQPage title="Results"><ResultsArticle /></FAQPage>
-      },
-      {
-        path: "evidence",
-        element: <FAQPage title="Evidence"><Evidence /></FAQPage>
-      },
-      {
-        path: "search-history",
-        element: <FAQPage title="Search History"><SearchHistoryArticle /></FAQPage>
-      },
-      {
-        path: "send-feedback",
-        element: <FAQPage title="Send Feedback"><SendFeedbackArticle /></FAQPage>
-      },
-      {
-        path: "results",
-        element: <Page title="Results"><Results /></Page>
-      },
-      {
-        path: "history",
-        element: <Page title="History"><History /></Page>
+        path: "main",
+        element: <Page title="Home"><Home /></Page>,
+        children: routes
       }
-    ],
+    ]
+
   },
 ]);
 
