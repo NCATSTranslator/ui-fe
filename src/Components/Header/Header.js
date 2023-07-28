490f8b64f29ca535cf83eab8610b375fc79f5e3d
David Smith <dnsmith124@gmail.com>
2023-07-28 10:13:26 -0500

    update header links to respect site root

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,17 +5,19 @@ import styles from './Header.module.scss';
 
 const Header = ({children, handleFeedbackModalOpen}) => {
   
+  let pathRoot = (window.location.pathname.includes("main")) ? 'main' : 'demo';
+
   return (
     <header className={styles.header}>
       <div className={styles.topBar}>
         <div className={styles.container}>
           <div className={styles.left}>
-            <Link to="/" className={styles.logo}><Logo/></Link>
+            <Link to={`/${pathRoot}/`} className={styles.logo}><Logo/></Link>
           </div>
           <div className={styles.right}>
-            <Link to="/history">Search History</Link>
+            <Link to={`/${pathRoot}/history`}>Search History</Link>
             <button onClick={()=>handleFeedbackModalOpen()}>Send Feedback</button>
-            <Link to="/help" className={styles.help} rel="noreferrer" target={'_blank'} >Help</Link>
+            <Link to={`/${pathRoot}/help`} className={styles.help} rel="noreferrer" target={'_blank'} >Help</Link>
           </div>
         </div>
       </div>
