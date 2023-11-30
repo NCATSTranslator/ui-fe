236ecb0d760ee4d057a13e75de4d3b1d92e55a21
David Smith <dnsmith124@gmail.com>
2023-11-30 13:08:51 -0600

    set up disclaimer to show on first general page visit, and on first login page visit

--- src/Components/Page/Page.jsx
+++ src/Components/Page/Page.jsx
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import DisclaimerModal from "../Modals/DisclaimerModal";
 
 const Page = ({title, children}) => {
+
+  const disclaimerApproved = JSON.parse(localStorage.getItem('disclaimerApproved'));
+  const loginDisclaimerApproved = JSON.parse(localStorage.getItem('loginDisclaimerApproved'));
+  const path = window.location.pathname;
+  const initDisclaimerApproval = (path.includes('login') ? loginDisclaimerApproved : disclaimerApproved);
+  const [isDisclaimerApproved, setIsDisclaimerApproved] = useState(initDisclaimerApproval);
+
   useEffect(() => {
     document.title = `${title} - NCATS Biomedical Data Translator`|| "";
   }, [title]);
+
+  const handleDisclaimerClose = () => {
+    let cookieName;
+    if(path.includes("login"))
+      cookieName = 'loginDisclaimerApproved';
+    else 
+      cookieName = 'disclaimerApproved';
+
+    localStorage.setItem(cookieName, true);
+    setIsDisclaimerApproved(true);
+  }
+  
   return (
     <>
       {children}
-      <DisclaimerModal></DisclaimerModal>
+      <DisclaimerModal
+        isOpen={!isDisclaimerApproved}
+        onClose={handleDisclaimerClose}   
+      />
     </>
   )
 };
