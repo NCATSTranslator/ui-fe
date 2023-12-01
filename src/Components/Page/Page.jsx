30663cbc7e48394999c3d50ee9669c09f9565710
David Smith <dnsmith124@gmail.com>
2023-12-01 10:23:21 -0600

    refactor disclaimer to support expiry date of 1 year on login disclaimer

--- src/Components/Page/Page.jsx
+++ src/Components/Page/Page.jsx
@@ -1,35 +1,44 @@
 import { useEffect, useState } from "react";
 import DisclaimerModal from "../Modals/DisclaimerModal";
 
-const Page = ({title, children}) => {
-
-  const disclaimerApproved = JSON.parse(localStorage.getItem('disclaimerApproved'));
-  const loginDisclaimerApproved = JSON.parse(localStorage.getItem('loginDisclaimerApproved'));
-  const path = window.location.pathname;
-  const initDisclaimerApproval = (path.includes('login') ? loginDisclaimerApproved : disclaimerApproved);
-  const [isDisclaimerApproved, setIsDisclaimerApproved] = useState(initDisclaimerApproval);
+const Page = ({ title, children }) => {
+  const [isDisclaimerApproved, setIsDisclaimerApproved] = useState(false);
 
   useEffect(() => {
-    document.title = `${title} - NCATS Biomedical Data Translator`|| "";
+    const disclaimerApproved = JSON.parse(localStorage.getItem('disclaimerApproved'));
+    const loginDisclaimer = localStorage.getItem('loginDisclaimerApproved');
+
+    let initDisclaimerApproval = disclaimerApproved;
+    if (window.location.pathname.includes('login') && loginDisclaimer) {
+      const { approved, timestamp } = JSON.parse(loginDisclaimer);
+      const oneYear = 365 * 24 * 60 * 60 * 1000; // one year in milliseconds
+      const isOlderThanOneYear = Date.now() - timestamp > oneYear;
+
+      if (isOlderThanOneYear) {
+        localStorage.removeItem('loginDisclaimerApproved');
+        initDisclaimerApproval = false;
+      } else {
+        initDisclaimerApproval = approved;
+      }
+    }
+
+    setIsDisclaimerApproved(initDisclaimerApproval);
+    document.title = `${title} - NCATS Biomedical Data Translator` || "";
   }, [title]);
 
   const handleDisclaimerClose = () => {
-    let cookieName;
-    if(path.includes("login"))
-      cookieName = 'loginDisclaimerApproved';
-    else 
-      cookieName = 'disclaimerApproved';
-
-    localStorage.setItem(cookieName, true);
+    const cookieName = window.location.pathname.includes("login") ? 'loginDisclaimerApproved' : 'disclaimerApproved';
+    const item = JSON.stringify({ approved: true, timestamp: Date.now() });
+    localStorage.setItem(cookieName, item);
     setIsDisclaimerApproved(true);
   }
-  
+
   return (
     <>
       {children}
       <DisclaimerModal
         isOpen={!isDisclaimerApproved}
-        onClose={handleDisclaimerClose}   
+        onClose={handleDisclaimerClose}
       />
     </>
   )
