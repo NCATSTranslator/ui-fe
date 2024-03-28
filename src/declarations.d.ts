d657dd1b08d05f3d5c84ea436b1eb07295e0781e
David Smith <dnsmith124@gmail.com>
2024-03-28 11:28:47 -0500

    add type declaration support for svg imports

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,9 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.svg?react' {
+  import React from 'react';
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
