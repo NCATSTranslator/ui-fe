08894c3d9b0d47ca4b4b675e8b5b5639d22650a4
David Smith <dnsmith124@gmail.com>
2024-04-08 11:51:03 -0500

    remove 'step' text from help pages

--- src/PageRoutes/Articles/ReviewIdentify.jsx
+++ src/PageRoutes/Articles/ReviewIdentify.jsx
@@ -11,13 +11,13 @@ export const ReviewIdentify = () => {
   return (
     <>
       <p className="caption">Last updated on September 8th, 2023</p>
-      <p><strong>Step 5: Review the result network.</strong> The knowledge graph representing the connections between a result and the search term are displayed with tools to explore.</p>
+      <p><strong>Review the result network.</strong> The knowledge graph representing the connections between a result and the search term are displayed with tools to explore.</p>
       <p style={{'text-align': 'center'}}><img src={reviewImage} alt="example showing a results graph" /></p>
       
-      <p><strong>Step 6: Explore the paths.</strong> Each network is broken out into every path through the network to explore individually. Each concept is displayed with its relationship to another concept.  Selecting the connecting relationships (affects, treats, or contributes to below) will display the evidence supporting that relationship.</p>
+      <p><strong>Explore the paths.</strong> Each network is broken out into every path through the network to explore individually. Each concept is displayed with its relationship to another concept.  Selecting the connecting relationships (affects, treats, or contributes to below) will display the evidence supporting that relationship.</p>
       <p style={{'text-align': 'center'}}><img src={pathsImage} alt="example showing the paths from a result" /></p>
 
-      <p><strong>Step 7: Filtering results.</strong> Results can be filtered to identify the most valuable to users.</p>
+      <p><strong>Filtering results.</strong> Results can be filtered to identify the most valuable to users.</p>
       <p>
         <ol>
           <li>The left panel provides facets to search and filter result sets.</li>  
@@ -27,11 +27,11 @@ export const ReviewIdentify = () => {
       </p>
       <p style={{'text-align': 'center'}}><img src={filterImage} alt="results page with filter bar on left" /></p>
 
-      <p><strong>Step 8: Select the results that are worth further study.</strong> The bookmark icon indicates if a result has been identified as a favorite. Either selecting the bookmark or utilizing the notes function (automatically activating the bookmark) will identify results for further study.</p> 
+      <p><strong>Select the results that are worth further study.</strong> The bookmark icon indicates if a result has been identified as a favorite. Either selecting the bookmark or utilizing the notes function (automatically activating the bookmark) will identify results for further study.</p> 
       <p>Selecting the bookmark icon (<Bookmark style={{"display":"inline", "width":"20px", "margin-bottom": "-4px"}} />) or creating a note about a result saves it for further review in the workspace.</p>
       <p style={{'text-align': 'center'}}><img src={bookmarkImage} alt="single result showing bookmark and notes icons" /></p>
 
-      <p><strong>Step 9: Evaluate the evidence supporting a relationship.</strong> Each relationship has evidence. This includes publications as well as other sources.</p>
+      <p><strong>Evaluate the evidence supporting a relationship.</strong> Each relationship has evidence. This includes publications as well as other sources.</p>
       <p>Support from publications are summarized in a table. Each relationship is capture in the table with information about and links to the publication. </p>
       <p style={{'text-align': 'center'}}><img src={pubsImage} alt="list of publications for a result" /></p>
       <p>Other sources are cited in the 'Sources' tab. The specific relationship is displayed with a link to a description of how the support was generated.</p>
