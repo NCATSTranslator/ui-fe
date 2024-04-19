5a0275f1f8c038b357724bd9588c5e68150a8267
Guthrie Price <guthrie.price@bmahq.com>
2024-04-19 11:05:08 -0700

    auto whitespace fix

--- src/Utilities/customHooks.js
+++ src/Utilities/customHooks.js
@@ -24,13 +24,13 @@ export const useWindowSize = (delay = 100) => {
         });
       }, delay);
     }
-    
+
     // Add event listener
     window.addEventListener("resize", handleResize);
-    
+
     // Call handler right away so state gets updated with initial window size
     handleResize();
-    
+
     // Remove event listener on cleanup
     return () => {
       if (timeoutId !== null) {
