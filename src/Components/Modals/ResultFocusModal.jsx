4753f8ff6e3d6901cc5928dba18286090d140eaa
David Smith <dnsmith124@gmail.com>
2024-04-30 11:17:10 -0500

    adjust styling and verbiage in ResultFocusModal

--- src/Components/Modals/ResultFocusModal.jsx
+++ src/Components/Modals/ResultFocusModal.jsx
@@ -4,11 +4,14 @@ import Button from '../FormFields/Button';
 import { formatBiolinkNode, truncateStringIfTooLong } from '../../Utilities/utilities';
 
 const ResultFocusModal = ({isOpen, onAccept, onReject, sharedItem}) => {
+
   const name = truncateStringIfTooLong(formatBiolinkNode(sharedItem.name, sharedItem.type));
+
   return (
     <Modal isOpen={isOpen} onClose={onReject} hideCloseButton={true}>
       <div className={styles.content}>
-        <h4 className={styles.heading}>The link that is loading is meant to share the result <span className={`${styles.heading} ${styles.sharedItem}`}> {name}</span>. Would you want to jump to it?</h4>
+        <h4 className={styles.heading}>Shared Link Loaded</h4>
+        <p>The link that just loaded is meant to share the following result:<span className={`${styles.sharedItem}`}> {name}</span>. Do you want to jump to it?</p>
       </div>
       <div className={styles.buttonsContainer}>
         <Button handleClick={onReject} className={styles.button} isSecondary>Do not jump to the result</Button>
