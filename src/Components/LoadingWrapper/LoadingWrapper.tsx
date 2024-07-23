3ac3b9e863fc423eaaf6a9d5d668d466b97c0a61
David Smith <dnsmith124@gmail.com>
2024-07-23 15:19:41 -0500

    refine LoadingWrapper with default delay, adjust anim duration

--- src/Components/LoadingWrapper/LoadingWrapper.tsx
+++ src/Components/LoadingWrapper/LoadingWrapper.tsx
@@ -9,16 +9,23 @@ interface LoadingWrapperProps {
 
 const LoadingWrapper: FC<LoadingWrapperProps> = ({ loading, children }) => {
   const [showContent, setShowContent] = useState(false);
+  const [isLoading, setIsLoading] = useState(loading);
+  const showDelay = 350;
 
   useEffect(() => {
+    let timer: boolean | NodeJS.Timeout = false;
     if (!loading) {
-      setShowContent(true);
+      timer = setTimeout(() => {setShowContent(true); setIsLoading(false);}, showDelay);
     }
+    return () => {
+      if(!!timer)
+       clearTimeout(timer);
+    };
   }, [loading]);
 
   return (
     <div>
-      <div className={`${styles.loadingContainer} ${loading ? styles.fadeEnterActive : styles.fadeExitActive}`}>
+      <div className={`${styles.loadingContainer} ${isLoading ? styles.fadeEnterActive : styles.fadeExitActive}`}>
         <LoadingIcon />
       </div>
       <div className={`${styles.contentContainer} ${showContent ? styles.fadeEnterActive : styles.fadeExitActive}`}>
