d98d8a6f456527073579503450ad7fa33c59a933
David Smith <dnsmith124@gmail.com>
2024-08-02 10:05:00 -0500

    update query bar styles

--- src/Components/QueryBar/QueryBar.jsx
+++ src/Components/QueryBar/QueryBar.jsx
@@ -3,7 +3,9 @@ import TextInput from "../Core/TextInput";
 import Autocomplete from "../Autocomplete/Autocomplete";
 import SearchIcon from '../../Icons/Buttons/Search.svg?react';
 import styles from './QueryBar.module.scss';
+import ArrowRight from "../../Icons/Directional/Arrows/Arrow Right.svg?react";
 import QueryTypeIcon from "../QueryTypeIcon/QueryTypeIcon";
+import Button from "../Core/Button";
 
 const QueryBar = ({handleSubmission, handleChange, queryType, value, autocompleteItems, 
   autocompleteLoading, handleItemClick, disabled = false}) => {
@@ -24,16 +26,21 @@ const QueryBar = ({handleSubmission, handleChange, queryType, value, autocomplet
         items={autocompleteItems}
         handleItemClick={handleItemClick}
       />
-      <TextInput 
-        placeholder={placeholderText} 
-        handleChange={(e)=>handleChange(e)} 
-        className={styles.input}
-        size=""
-        iconLeft={<QueryTypeIcon type={queryType.searchTypeString}/>}
-        iconRight={<SearchIcon/>}
-        value={value}
-        disabled={disabled}
-      />
+      <div className={styles.inputContainer}>
+        <span className={styles.icon}>
+          <QueryTypeIcon type={queryType.searchTypeString}/>
+        </span>
+        {/* <input type="text" placeholder={placeholderText} handleChange={(e)=>handleChange(e)} className={styles.input} value={value} disabled={disabled} /> */}
+        <TextInput 
+          placeholder={placeholderText} 
+          handleChange={(e)=>handleChange(e)} 
+          className={styles.input}
+          size=""
+          value={value}
+          disabled={disabled}
+        />
+        <Button handleClick={handleSubmission} className={styles.submitButton} iconOnly><ArrowRight/></Button>
+      </div>
     </form>
   )
 }
