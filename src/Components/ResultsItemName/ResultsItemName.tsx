699363dc49341b0ea58fcbdb3d0abfdb3a1a93d8
David Smith <dnsmith124@gmail.com>
2024-09-06 09:35:32 -0500

    correct capitalization issue in some PF results

--- src/Components/ResultsItemName/ResultsItemName.tsx
+++ src/Components/ResultsItemName/ResultsItemName.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import styles from './ResultsItemName.module.scss';
-import { ResultItem } from "../../Types/results";
-import { getIcon } from "../../Utilities/utilities";
+import { RawNode, ResultItem } from "../../Types/results";
+import { formatBiolinkNode, getIcon } from "../../Utilities/utilities";
 import Highlighter from "react-highlight-words";
 import ArrowIcon from "../../Icons/Directional/Arrows/Arrow Right.svg?react";
 import { getFormattedPathfinderName } from "../../Utilities/resultsFormattingFunctions";
@@ -21,6 +21,17 @@ const ResultsItemName: FC<ResultsItemNameProps> = ( {isPathfinder = false, item,
   const subjectIcon: JSX.Element | null = item.subjectNode?.types.length > 0 ? getIcon(item.subjectNode.types[0]) : null;
   const objectIcon: JSX.Element | null = item.objectNode?.types.length > 0 ? getIcon(item.objectNode.types[0]) : null;
 
+  const getPFNameString = (index: number, isNotLastItem: boolean, name: string, subject: RawNode, object: RawNode ) => {
+    let pfNameString = "";
+    if(index === 0 || !isNotLastItem) {
+      let type = (index === 0) ? subject.types[0]: object.types[0];
+      pfNameString = formatBiolinkNode(name, type);
+    } else {
+      pfNameString = getFormattedPathfinderName(name);
+    }
+    return pfNameString;
+  }
+
   return (
     <>
       {
@@ -30,7 +41,7 @@ const ResultsItemName: FC<ResultsItemNameProps> = ( {isPathfinder = false, item,
             {
               pathfinderNameArray?.map( (name, i) => {
                 const isNotLastItem = (i < pathfinderNameArray.length - 1);
-                const pfNameString = (i!== 0 && isNotLastItem) ? getFormattedPathfinderName(name) : name;
+                const pfNameString = getPFNameString(i, isNotLastItem, name, item.subjectNode, item.objectNode);
                 const originalBiolinkName = `biolink:${pfNameString.replaceAll(" ", "_")}`;
                 return (
                   <span className={styles.nameContainer}>
