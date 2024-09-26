2d0b027f4938a5a3b025b94c9ada42766625ffbe
David Smith <dnsmith124@gmail.com>
2024-09-26 10:06:25 -0500

    update help pages with new screenshots, adjust some language

--- src/PageRoutes/Articles/UserPreferences.jsx
+++ src/PageRoutes/Articles/UserPreferences.jsx
@@ -4,7 +4,7 @@ export const UserPreferences = () => {
 
   return (
     <>
-      <p className="caption">Last updated on September 8th, 2023</p>
+      <p className="caption">Last updated on September 25th, 2024</p>
       <p>The interface has a number of settings that can be adjusted based on a user's preferences.  The defaults for those settings can be accessed here on the 'User Preferences' page. This page is accessed by clicking the user icon to the left of the 'Log Out' button.</p>
       <p style={{'text-align': 'center'}}><img src={userprefsImage} alt="list of user preferences" /></p>
     </>
--- src/PageRoutes/Articles/Workspace.jsx
+++ src/PageRoutes/Articles/Workspace.jsx
@@ -4,7 +4,7 @@ export const WorkspaceHelp = () => {
 
   return (
     <>
-      <p className="caption">Last updated on September 8th, 2023</p>
+      <p className="caption">Last updated on September 25th, 2024</p>
       <p>The workspace retains all results that have been bookmarked. Other results are removed to allow focused review.</p>
       <p style={{'text-align': 'center'}}><img src={workspaceImage} alt="bookmarks displayed on the workspace page" /></p>
     </>
