888e0f6c363ae850eb89991d31ce175177a71a1d
David Smith <dnsmith124@gmail.com>
2024-12-09 15:33:00 -0600

    convert evidenceModalFunctions to ts

--- src/Utilities/evidenceModalFunctions.ts
+++ src/Utilities/evidenceModalFunctions.ts
@@ -1,20 +1,26 @@
+import { Dispatch, SetStateAction } from 'react';
 import { sortNameHighLow, sortNameLowHigh, sortJournalHighLow, sortJournalLowHigh,
   sortDateYearHighLow, sortDateYearLowHigh } from './sortingFunctions';
 import { cloneDeep } from 'lodash';
 import { capitalizeAllWords } from "./utilities";
+import { EvidenceSortState, PublicationObject, RawPublicationList } from '../Types/evidence';
+import { ResultEdge, ResultSet } from '../Types/results';
+import { getPubById } from '../Redux/resultsSlice';
 
-export const handleEvidenceSort = (sortName, pubmedEvidence, handlePageClick, sortingStateSetter, setPubmedEvidence) => {
+export const handleEvidenceSort = (sortName: string, pubmedEvidence: PublicationObject[], handlePageClick: (event: any) => void, 
+  sortingStateSetter: Dispatch<SetStateAction<EvidenceSortState>>, setPubmedEvidence: Dispatch<SetStateAction<PublicationObject[]>>) => {
+
   let sortedPubmedEvidence = cloneDeep(pubmedEvidence);
-  let newSortingState = { title: null, journal: null, date: null };
+  let newSortingState: EvidenceSortState = { title: null, journal: null, date: null };
   switch (sortName) {
     case 'titleLowHigh':
-      sortedPubmedEvidence = sortNameLowHigh(sortedPubmedEvidence, true);
+      sortedPubmedEvidence = sortNameLowHigh(sortedPubmedEvidence) as PublicationObject[];
       newSortingState.title = true;
       newSortingState.journal = null;
       newSortingState.date = null;
       break;
     case 'titleHighLow':
-      sortedPubmedEvidence = sortNameHighLow(sortedPubmedEvidence, true);
+      sortedPubmedEvidence = sortNameHighLow(sortedPubmedEvidence) as PublicationObject[];
       newSortingState.title = false;
       newSortingState.journal = null;
       newSortingState.date = null;
@@ -48,42 +54,41 @@ export const handleEvidenceSort = (sortName, pubmedEvidence, handlePageClick, so
   }
 
   sortingStateSetter(newSortingState);
-  // assign the newly sorted results (no need to set formatted results, since they'll be filtered after being sorted, then set there)
   setPubmedEvidence(sortedPubmedEvidence);
 
   // reset to page one.
   handlePageClick({selected: 0});
 }
 
-export const checkForEdgeMatch = (edgeOne, edgeTwo) => {
+export const checkForEdgeMatch = (edgeOne: ResultEdge | null, edgeTwo: ResultEdge | null) => {
   return (!edgeOne || !edgeTwo ||
-    edgeOne.edges[0].subject.id !== edgeTwo.edges[0].subject.id ||
+    edgeOne.subject !== edgeTwo.subject ||
     edgeOne.predicate !== edgeTwo.predicate || 
-    edgeOne.edges[0].object.id !== edgeTwo.edges[0].object.id) 
+    edgeOne.object !== edgeTwo.object) 
     ? false
     : true;
 }
 
-export const updateJournal = (element, data) => {
-  if(!element.journal)
+export const updateJournal = (element: PublicationObject, data: any) => {
+  if(!element.journal && element.id)
     element.journal = capitalizeAllWords(data[element.id].journal_name);
 }
-export const updateTitle = (element, data) => {
-  if(!element.title)
+export const updateTitle = (element: PublicationObject, data: any) => {
+  if(!element.title && element.id)
     element.title = capitalizeAllWords(data[element.id].article_title.replace('[', '').replace(']',''));
 }
-export const updateSnippet = (element, data) => {
-  if(!element.snippet)
+export const updateSnippet = (element: PublicationObject, data: any) => {
+  if(!element.snippet && element.id)
     element.snippet = data[element.id].abstract;
 }
-export const updatePubdate = (element, data) => {
-  if(!element.pubdate) {
+export const updatePubdate = (element: PublicationObject, data: any) => {
+  if(!element.pubdate && element.id) {
     let year = (data[element.id].pub_year) ? data[element.id].pub_year: 0;
     element.pubdate = year;
   }
 }
 
-export const getKnowledgeLevelString = (knowledgeLevel) => {
+export const getKnowledgeLevelString = (knowledgeLevel: string): string => {
   let knowledgeLevelString;
   switch (knowledgeLevel) {
     case 'trusted':
@@ -99,11 +104,35 @@ export const getKnowledgeLevelString = (knowledgeLevel) => {
   return knowledgeLevelString;
 }
 
-export const generatePubmedURL = (id) => {
+export const generatePubmedURL = (id: string): string => {
   if(id.includes("PMC")) 
     return `https://www.ncbi.nlm.nih.gov/pmc/${id}`;
   if(id.includes("PMID"))
     return `http://www.ncbi.nlm.nih.gov/pubmed/${id.replace("PMID:", "")}`;
 
   return "";
+}
+
+export const flattenPublicationsObject = (resultSet: ResultSet | null, pubs: RawPublicationList): PublicationObject[] => {
+  const pubArray: PublicationObject[] = [];
+  if(!resultSet)
+    return pubArray;
+
+  for (const key in pubs) {
+    const entries = pubs[key];
+    for (const entryID of entries) {
+      const pub = getPubById(resultSet, entryID.id);
+      if(!!pub) {
+        pubArray.push({
+          knowledgeLevel: key, 
+          id: entryID.id,
+          source: pub.source,
+          support: pub.support || null,
+          type: pub.type,
+          url: pub.url
+        });
+      }
+    }
+  }
+  return pubArray;
 }
