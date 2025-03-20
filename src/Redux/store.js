1256200a84ba05e0ec6f71974a79375b0bdf9b63
David Smith <dnsmith124@gmail.com>
2025-03-20 10:11:34 -0500

    reenable reduxState localStorage to fix bug with search history

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -12,3 +12,58 @@ export const store = configureStore({
     root: rootReducer
   },
 })
+
+// Utility functions/variables for store.subscribe() callback function handleStoreUpdate
+const getRoot = (state) => state.root.currentRoot;
+const getHistory = (state) => state.history.pastQueries;
+const getCurrentQuery = (state) => state.query.currentQuery;
+ 
+let currentRootValue;
+let currentHistoryValue;
+let currentQueryValue;
+ 
+// Callback that logs state changes to console through store.subscribe() 
+const handleStoreUpdate = () => {
+  let previousRootValue = currentRootValue;
+  currentRootValue = getRoot(store.getState());
+  if (previousRootValue !== currentRootValue
+    && previousRootValue !== undefined) {
+    console.log(
+      'Current root has changed from ',
+      previousRootValue,
+      'to',
+      currentRootValue
+    )
+  }
+
+  // Query History
+  let previousHistoryValue = currentHistoryValue;
+  currentHistoryValue = getHistory(store.getState());
+  if (previousHistoryValue !== currentHistoryValue
+    && previousHistoryValue !== undefined) {
+    console.log(
+      'Query history has changed from ',
+      previousHistoryValue,
+      'to',
+      currentHistoryValue
+    )
+  }
+
+  // Current Query
+  let previousQueryValue = currentQueryValue;
+  currentQueryValue = getCurrentQuery(store.getState());
+  if (previousQueryValue !== currentQueryValue
+    && previousQueryValue !== undefined) {
+    console.log(
+      'Current query has changed from ',
+      previousQueryValue,
+      'to',
+      currentQueryValue
+    )
+  }
+} 
+
+store.subscribe(() => {
+  handleStoreUpdate();
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+})
