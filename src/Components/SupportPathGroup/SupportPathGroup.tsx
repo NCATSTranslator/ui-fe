482f29d3c4857d7517d861193217903c4c92bac4
David Smith <dnsmith124@gmail.com>
2025-05-07 14:48:25 -0500

    remove node click link out, rely instead on link in tooltip

--- src/Components/SupportPathGroup/SupportPathGroup.tsx
+++ src/Components/SupportPathGroup/SupportPathGroup.tsx
@@ -115,7 +115,6 @@ const SupportPathGroup: FC<SupportPathGroupProps> = ({
                   return null;
                 const indexInFullCollection = itemOffset + i;
                 const pathKey = currentDepth === 2 ? intToNumeral(indexInFullCollection + 1) : intToChar(indexInFullCollection + 1);
-                const fullPathKey = `${parentKey}.${pathKey}`;
                 return (
                   <SupportPath
                     key={supportPath.id}
