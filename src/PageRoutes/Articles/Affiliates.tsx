e520ebd5886c084ab8de9b7f81a3d17857247582
David Smith <dnsmith124@gmail.com>
2025-06-30 09:20:20 -0500

    Refresh Git cache

--- src/PageRoutes/Articles/Affiliates.tsx
+++
@@ -1,8 +0,0 @@
-export const Affiliates = () => {
-  return (
-    <>
-      <p className="caption">Last updated on September 6th, 2022</p>
-      <p>This work was supported by the NCATS Biomedical Data Translator Program (Other Transaction Awards OT2TR003434, OT2TR003436, OT2TR003428, OT2TR003448, OT2TR003427, OT2TR003430, OT2TR003433, OT2TR003450, OT2TR003437, OT2TR003443, OT2TR003441, OT2TR003449, OT2TR003445, OT2TR003422, OT2TR003435, OT3TR002026, OT3TR002020, OT3TR002025, OT3TR002019, OT3TR002027, OT2TR002517, OT2TR002514, OT2TR002515, OT2TR002584, OT2TR002520; Contract number 75N95024F00144). Additional funding was provided by the Intramural Research Program at NCATS (ZIA TR000276-05).</p>
-    </>
-  );
-}
