2c5be0bd413f8f34f003586a9011a9bbf73d2f70
David Smith <dnsmith124@gmail.com>
2025-10-06 09:45:16 -0500

    update settings panel icon

--- src/features/Sidebar/utils/sidebarItems.tsx
+++ src/features/Sidebar/utils/sidebarItems.tsx
@@ -2,7 +2,7 @@ import { SidebarItem } from "@/features/Sidebar/types/sidebar";
 import SearchIcon from '@/assets/icons/buttons/Search.svg?react';
 import FolderIcon from '@/assets/icons/projects/folder.svg?react';
 import HistoryIcon from '@/assets/icons/navigation/History.svg?react';
-import Cog from '@/assets/icons/navigation/Settings.svg?react';
+import UserIcon from '@/assets/icons/projects/user.svg?react';
 import Feedback from '@/assets/icons/navigation/Feedback.svg?react';
 import Question from '@/assets/icons/navigation/Help.svg?react';
 import ProjectsPanel from "@/features/Sidebar/components/Panels/ProjectsPanel/ProjectsPanel";
@@ -29,5 +29,5 @@ export const topItems: SidebarItem[] = [
 export const bottomItems: SidebarItem[] = [
   { id: 'feedback', label: 'Send Feedback', type: 'link', to: '/send-feedback', icon: <Feedback />, tooltipText: 'Send Feedback' },
   { id: 'help', label: 'Help', type: 'link', to: '/help', icon: <Question />, tooltipText: 'Help' },
-  { id: 'settings', label: 'Settings', type: 'panel', icon: <Cog />, tooltipText: 'Settings', panelComponent: <SettingsPanel /> },
+  { id: 'settings', label: 'Settings', type: 'panel', icon: <UserIcon />, tooltipText: 'Settings', panelComponent: <SettingsPanel /> },
 ];
