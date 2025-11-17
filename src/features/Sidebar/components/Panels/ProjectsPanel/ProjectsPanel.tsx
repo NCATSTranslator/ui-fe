390cb680e02fdf114aff0720d7dd7d81533c50ac
David Smith <dnsmith124@gmail.com>
2025-11-17 10:36:27 -0600

    simplify active project id calc in ProjectsPanel by using useParams

--- src/features/Sidebar/components/Panels/ProjectsPanel/ProjectsPanel.tsx
+++ src/features/Sidebar/components/Panels/ProjectsPanel/ProjectsPanel.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from "react";
-import { matchPath, useLocation } from "react-router-dom";
+import { matchPath, useLocation, useParams } from "react-router-dom";
 import styles from "./ProjectsPanel.module.scss";
 import { useSelector } from "react-redux";
 import { currentUser } from "@/features/UserAuth/slices/userSlice";
@@ -31,10 +31,8 @@ const ProjectsPanel = () => {
   const [newProjectId, setNewProjectId] = useState<number | null>(null);
   const { addToProjectQuery, clearAddToProjectMode } = useSidebar();
   const { title: queryTitle } = useGetQueryCardTitle(addToProjectQuery || null);
-  const activeProjectId = useMemo(() => {
-    const match = matchPath('/projects/:projectId', location.pathname);
-    return match?.params.projectId ? Number(match.params.projectId) : null;
-  }, [location.pathname]);
+  const { projectId } = useParams<{ projectId: string }>();
+  const activeProjectId = projectId ? Number(projectId) : null;
   
   const filteredProjects = useMemo(() => {
     if(searchTerm.length === 0) return projects;
