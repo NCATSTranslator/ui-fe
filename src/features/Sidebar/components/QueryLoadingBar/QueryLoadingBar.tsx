9b26245566609741c2d236abc865fc1abf98d31d
David Smith <dnsmith124@gmail.com>
2025-12-19 14:36:24 -0600

    add hide prop to conditionally hide the query loading bar

--- src/features/Sidebar/components/QueryLoadingBar/QueryLoadingBar.tsx
+++ src/features/Sidebar/components/QueryLoadingBar/QueryLoadingBar.tsx
@@ -4,13 +4,14 @@ import styles from "./QueryLoadingBar.module.scss";
 interface QueryLoadingBarProps {
   fillPercentage: number;
   full?: boolean;
+  hide?: boolean;
 }
 
-const QueryLoadingBar: FC<QueryLoadingBarProps> = ({ fillPercentage, full = false }) => {
+const QueryLoadingBar: FC<QueryLoadingBarProps> = ({ fillPercentage, full = false, hide = false }) => {
   return (
-    <div className={styles.loadingBarContainer}>
+    <div className={`${styles.loadingBarContainer} ${hide ? styles.hide : ''}`}>
       <div 
-        className={`${styles.loadingBarFill} ${full && styles.full}`}
+        className={`${styles.loadingBarFill} ${full ? styles.full : ''}`}
         style={{ width: `${Math.min(Math.max(fillPercentage, 0), 100)}%` }}
       />
     </div>
