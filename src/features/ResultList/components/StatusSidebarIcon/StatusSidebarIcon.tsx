dd442390d5d411b5b65e130155ac981b17d38612
David Smith <dnsmith124@gmail.com>
2026-02-24 11:48:46 -0600

    fix padding in Filter sidebar panel, add result sync button to new result tooltip

--- src/features/ResultList/components/StatusSidebarIcon/StatusSidebarIcon.tsx
+++ src/features/ResultList/components/StatusSidebarIcon/StatusSidebarIcon.tsx
@@ -4,11 +4,13 @@ import StatusIndicator from "@/features/Projects/components/StatusIndicator/Stat
 import Button from "@/features/Core/components/Button/Button";
 import CloseIcon from "@/assets/icons/buttons/Close/Close.svg?react";
 import Checkbox from "@/features/Core/components/Checkbox/Checkbox";
-import { ARAStatusResponse } from "@/features/ResultList/types/results";
+import { ARAStatusResponse, ResultListLoadingData } from "@/features/ResultList/types/results.d";
 import { getQueryStatusPercentage } from "@/features/Projects/utils/utilities";
+import ResultListLoadingButton from "@/features/ResultList/components/ResultListLoadingButton/ResultListLoadingButton";
 
 interface StatusSidebarIconProps {
   arsStatus: ARAStatusResponse | null;
+  data: ResultListLoadingData;
   hasFreshResults: boolean;
   setShowQueryStatusToast: (show: boolean) => void;
   showQueryStatusToast: boolean;
@@ -16,6 +18,7 @@ interface StatusSidebarIconProps {
 }
 const StatusSidebarIcon: FC<StatusSidebarIconProps> = ({
   arsStatus,
+  data,
   hasFreshResults,
   setShowQueryStatusToast,
   showQueryStatusToast,
@@ -49,7 +52,14 @@ const StatusSidebarIcon: FC<StatusSidebarIconProps> = ({
         showToast && (
           <div className={`${styles.freshResultsToast} freshResultsToast`} onClick={(e) => e.stopPropagation()}>
             <span className={styles.toastHeading}>New Results Available!</span>
-            <span className={styles.toastText}>Click the status tab to learn about and sync new results.</span>
+            <span className={styles.toastText}>Click the status tab or the button below to learn about and sync new results.</span>
+            <ResultListLoadingButton 
+              hasFreshResults={data.hasFreshResults}
+              showDisclaimer={data.showDisclaimer}
+              handleResultsRefresh={data.handleResultsRefresh}
+              className={styles.loadingButton}
+              showIcon={false}
+            />
             <span className={styles.checkboxContainer}><Checkbox handleClick={handleCheckboxClick} className={styles.checkbox} />Don't show this again</span>
             <Button iconOnly iconLeft={<CloseIcon />} className={styles.closeButton} variant="textOnly" handleClick={handleCloseToast} />
           </div>
