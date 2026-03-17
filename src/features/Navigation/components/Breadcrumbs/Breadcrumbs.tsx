d151183033815b4fd70df31ead6f577bfafc4564
David Smith <dnsmith124@gmail.com>
2026-03-17 16:11:32 -0500

    Add backward navigation to the breadcrumb arrow

--- src/features/Navigation/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/features/Navigation/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import styles from './Breadcrumbs.module.scss';
-import { useMatches, useSearchParams } from 'react-router-dom';
+import { useMatches, useNavigate, useSearchParams } from 'react-router-dom';
 import { BreadcrumbHandle } from '@/features/Navigation/types/navigation.d';
 import BreadcrumbLink from '@/features/Navigation/components/BreadcrumbLink/BreadcrumbLink';
 import ArrowLeftIcon from '@/assets/icons/directional/Arrows/Arrow Left.svg?react';
@@ -8,7 +8,7 @@ import ArrowLeftIcon from '@/assets/icons/directional/Arrows/Arrow Left.svg?reac
 const Breadcrumbs: FC = () => {
   const matches = useMatches();
   const [searchParams] = useSearchParams();
-
+  const navigate = useNavigate();
   const crumbs = matches
     .filter((match) => (match.handle as BreadcrumbHandle)?.breadcrumb)
     .map((match) => ({
@@ -20,7 +20,7 @@ const Breadcrumbs: FC = () => {
 
   return (
     <nav aria-label="Breadcrumb" className={styles.breadcrumbs}>
-      <ArrowLeftIcon className={styles.arrowLeftIcon} />
+      <ArrowLeftIcon className={styles.arrowLeftIcon} onClick={() => navigate(-1)} />
       <ol className={styles.list}>
         {crumbs.map((crumb, index) => {
           const isLast = index === crumbs.length - 1;
